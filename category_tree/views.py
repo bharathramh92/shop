57fae58d748117165e7b042a3cd06146a60f21e4
bharath <bharathramh92@gmail.com>
2016-03-16 01:26:34 -0400

    refined formatting

--- category_tree/views.py
+++ category_tree/views.py
@@ -1,12 +1,18 @@
-from django.shortcuts import render
-from django.core import serializers
-from category_tree.models import Category
 import json
 from collections import defaultdict
 
+from django.shortcuts import render
+from django.core.urlresolvers import reverse
+from django.core import serializers
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from category_tree.models import Category
+
+
 def reset_view(request):
+    if not (request.user.is_authenticated() and request.user.is_superuser):
+        # only the admin should be able to access this page
+        return HttpResponseRedirect(reverse('listing:index'))
 
     data = serializers.serialize('json', Category.objects.all())
     data = json.loads(data)
@@ -26,24 +32,26 @@ def reset_view(request):
     with open("category_tree/categories.py", "w", encoding="utf-8") as out_file:
 
         def get_store_names():
-            return [data_map[d]["category_name"] for d in tree.keys()]
+            return {d: data_map[d]["category_name"] for d in tree.keys()}
+
+        end_categories_map = defaultdict(list)
 
         def get_store_end_categories():
 
-            end_categories = []
-
-            def go(dc):
+            def go(dc, store_name):
                 for k, v in dc.items():
                     if not v:
-                        end_categories.append((k, data_map[k]["category_name"]))
-                    go(v)
+                        end_categories_map[store_name].append((k, data_map[k]["category_name"]))
+                    go(v, store_name)
             for store in tree.keys():
-                go(tree[store])
-            return end_categories
+                go(tree[store], store)
+            return dict(end_categories_map)
 
+        store_buffer = get_store_names()
         data_str = "data = " + str(data_map) + "\n"
-        store_names = "store_names = " + str(get_store_names()) + "\n"
-        final_categories = "final_categories = " + str(get_store_end_categories()) + "\n"
+        store_names = "store_names = " + str(store_buffer) + "\n"
+        store_names_reverse_map = "store_names_reverse_map = " + str({v: k for k, v in store_buffer.items()}) + "\n"
+        final_categories_map = "final_categories = " + str(get_store_end_categories()) + "\n"
         tree = "tree = " + str(tree) + "\n"
-        out_file.write(data_str + store_names + final_categories + tree)
+        out_file.write(data_str + store_names + store_names_reverse_map + final_categories_map + tree)
     return render(request, "category_tree/reset_successful.html", {})
